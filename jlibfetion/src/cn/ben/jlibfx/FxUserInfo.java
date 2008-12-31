10c989d27a9b60f5fc3d4938f7b154a4ba5ff4a8
wenbin-pang@163.com <wenbin-pang@163.com@af657934-d6ff-11dd-a297-4fb579711a53>
2008-12-31 10:11:08 +0000

    '整理SVN项目目录结构'

---
+++ jlibfetion/src/cn/ben/jlibfx/FxUserInfo.java
@@ -0,0 +1,10 @@
+
+package cn.ben.jlibfx;
+
+/**
+ * Fetion 用户信息接口
+ * @author Ben.Pang
+ */
+public interface FxUserInfo {
+
+}
---
+++ jlibfetion/src/cn/ben/jlibfx/MessageListener.java
@@ -0,0 +1,15 @@
+
+package cn.ben.jlibfx;
+
+/**
+ * jLIbFx消息监听器基准接口,所有消息侦听器必须直接或间接继承此接口
+ * @author Ben.Pang
+ */
+public interface MessageListener extends java.util.EventListener {
+    /**
+     * 当接收到消息时调用,并传入该 {@link MessageEvent 消息事件对象}
+     * @see MessageEvent
+     * @param event {@link MessageEvent 消息事件对象}
+     */
+    public void messageReceived(MessageEvent event);
+}
