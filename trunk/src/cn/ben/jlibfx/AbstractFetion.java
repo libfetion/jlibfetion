d2cb3522a92e2ba851ea99a7097ffe80ef8e87ca
wenbin-pang@163.com <wenbin-pang@163.com@af657934-d6ff-11dd-a297-4fb579711a53>
2009-01-12 08:58:12 +0000

    J库基本框架基本完成,下一步编写实现类

--- trunk/src/cn/ben/jlibfx/AbstractFetion.java
+++ trunk/src/cn/ben/jlibfx/AbstractFetion.java
@@ -10,14 +10,14 @@ import java.util.ArrayList;
  * @author Ben.Pang
  */
 public abstract class AbstractFetion implements FxFetion {
-    protected FxAccount localAccount;                       // 本地帐户
-    protected ArrayList<ServerMsgListener> listeners;       // 服务器消息监听器
-    protected FxGroupsManager buddyGroups;                  // 好友组管理器
-    protected FxGroupsManager groups;                       // 群组管理器
-    protected FxGroup blackListGroup;                       // 黑名单
+    private FxAccount localAccount;                       // 本地帐户
+    protected ArrayList<ServerMsgListener> listeners;     // 服务器消息监听器
+    private FxGroupsManager buddyGroups;                  // 好友组管理器
+    private FxGroupsManager groups;                       // 群组管理器
+    private FxGroup blackListGroup;                       // 黑名单
 
     /**
-     * 注册服务器消息监听器. 如果 listener 为 null,则不抛出任何异常且不执行任何操作。
+     * 注册服务器消息监听器. 如果 listener 为 null,则不抛出任何异常且不执行任何操作.
      * @see ServerMsgListener
      * @param listener 消息监听器
      */
@@ -39,7 +39,7 @@ public abstract class AbstractFetion implements FxFetion {
         return arrays;
     }
     /**
-     * 移除服务器消息监听器. 如果 listener 为 null,则不抛出任何异常且不执行任何操作。
+     * 移除服务器消息监听器. 如果 listener 为 null,则不抛出任何异常且不执行任何操作.
      * @see ServerMsgListener
      * @param listener 将被移除的服务器消息监听器
      */
@@ -56,6 +56,14 @@ public abstract class AbstractFetion implements FxFetion {
         return buddyGroups;
     }
     /**
+     * 设置好友组管理器
+     * @see FxGroupsManager
+     * @param groups 好友组管理器
+     */
+    void setBuddyGroupsManager(FxGroupsManager groups) {
+        this.buddyGroups = groups;
+    }
+    /**
      * 返回群组管理器
      * @see FxGroupsManager
      * @return 群组管理器
@@ -64,11 +72,27 @@ public abstract class AbstractFetion implements FxFetion {
         return groups;
     }
     /**
+     * 设置群组管理器
+     * @see FxGroupsManager
+     * @param groups 群组管理器
+     */
+    void setGroupsManager(FxGroupsManager groups) {
+        this.groups = groups;
+    }
+    /**
      * 返回黑名单组
      * @return 黑名单组
      */
     public FxGroup getBlackList() {
         return blackListGroup;
+    }
+    /**
+     * 设置黑名单组
+     * @see FxGroupsManager
+     * @param groups 黑名单组
+     */
+    void setBlackList(FxGroup group) {
+        this.blackListGroup = group;
     }
     /**
      * 返回本地账户的状态信息
@@ -94,7 +118,7 @@ public abstract class AbstractFetion implements FxFetion {
      * @return {@link FxDialog 即时信息对话}
      * @throws cn.ben.jlibfx.FxException
      */
-    protected abstract FxDialog beginDialog(FxAccount account) throws FxException;
+    abstract FxDialog beginDialog(FxAccount account) throws FxException;
     /**
      * 开启一个新的 {@link FxDialog 组即时信息对话}
      * @see FxDialog
@@ -102,11 +126,11 @@ public abstract class AbstractFetion implements FxFetion {
      * @return {@link FxDialog 组即时信息对话}
      * @throws cn.ben.jlibfx.FxException
      */
-    protected abstract FxDialog beginDialog(FxGroup group) throws FxException;
+    abstract FxDialog beginDialog(FxGroup group) throws FxException;
     /**
      *  结束 {@link FxDialog 即时信息对话}
      * @param dialog {@link FxDialog 组即时信息对话}
      * @throws cn.ben.jlibfx.FxException
      */
-    protected abstract void endDialog(FxDialog dialog) throws FxException;
+    abstract void endDialog(FxDialog dialog) throws FxException;
 }
